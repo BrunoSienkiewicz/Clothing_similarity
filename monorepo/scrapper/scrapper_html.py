a171f77da87bce06be19129d8785a38aee73948b
Maks <makser.hofman@gmail.com>
2024-08-05 00:31:48 +0200

    UBR-14. Dodanie scrapper (nie dziala jeszcze)

---
+++ monorepo/scrapper/scrapper_html.py
@@ -0,0 +1,110 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
+import argparse
+import threading
+import pandas as pd
+
+"""
+flagi = link(defoult ta stronka od ubran), ilesecScroll, kategoria(z multi porcesingiem) 
+"""
+GRAILED_BASE_URL = "https://www.grailed.com/categories/"
+
+data = {
+    'listing_link': [],
+    'img_link': [],
+    'time_of_scrapping': [],
+    'listing_meta_data': [],
+    'listing_title': [],
+    'listing_price': [],
+    'see_similat_link': []
+}
+df = pd.DataFrame(data)
+def init_of_driver(url):
+    chrome_options = Options()
+    chrome_options.add_argument('--disable-logging')
+    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    #assert "Grailed" in driver.title
+    return driver
+
+def scroll_for_set_amout_of_sec(driver, secScroll):
+    end_time = time.time() + secScroll
+    while time.time() < end_time:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(0.1)
+
+def bfsoup_parse_of_html_and_add_to_df(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    #print(soup.prettify())
+    feed_items = soup.find_all(class_='feed-item')
+    for feed_item in feed_items:
+        print(feed_item)
+        listing_link = feed_item.find('a', class_='listing-item-link')
+        for links in listing_link:
+            print(links.get('href'))
+        listing_link = listing_link.get('href')
+        see_similat_link = feed_item.find('a', target='_blank')
+        see_similat_link = see_similat_link.get('href')
+        print(listing_link)
+        print(see_similat_link)
+def scrape_html(link, secScroll, categorie):
+    cookies_reject_all_id = 'onetrust-reject-all-handler'
+    login_widget_xpath = '/html/body/div[9]/div/div/div/div[2]/div'
+    random_xpath ='/html/body/div[9]/div'
+    url = link + categorie
+    driver = init_of_driver(url)
+    """
+    try:
+        reject_button = WebDriverWait(driver, 11).until(EC.element_to_be_clickable((By.ID, cookies_reject_all_id)))
+        reject_button.click()
+        reject_button.click()
+    except Exception as e:
+        pass
+    """
+    #WebDriverWait(driver, 11).until(EC.visibility_of_element_located((By.CLASS_NAME, 'feed-item')))
+    scroll_for_set_amout_of_sec(driver, secScroll)
+    parent_element = driver.find_element(By.CLASS_NAME, "feed")
+    html_content = parent_element.get_attribute('outerHTML')
+    bfsoup_parse_of_html_and_add_to_df(html_content)
+
+
+
+def scrape_main_func(link, secScroll, categories):
+    if link is None:
+        link = GRAILED_BASE_URL
+    if secScroll is None:
+        secScroll = 5
+    if categories is None:
+        categories = ["all"]
+    threads = []
+
+    for cat in categories:
+        thread = threading.Thread(target=scrape_html(link, secScroll, cat))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+
+def scrappe_site():
+    # Create the parser
+    parser = argparse.ArgumentParser(description="A simple script to demonstrate argparse.")
+
+    # Add a flag -f with a required argument
+    parser.add_argument('-link', '--link', type=str, required=False, help='a link to site defoult https://www.grailed.com/categories ')
+    parser.add_argument('-secScroll', '--secScroll', type=int, required=False, help='how long to scroll')
+    parser.add_argument('-categories', '--categories', type=str, required=False, help='pick categories you want to scrape from ')
+    args = parser.parse_args()
+    scrape_main_func(args.link, args.secScroll, args.categories)
+
+
+if __name__ == "__main__":
+    scrappe_site()
