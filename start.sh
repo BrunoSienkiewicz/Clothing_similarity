9f154b90f3b23605659a4df1c3939460d7eb4330
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-08-07 21:28:29 +0200

    update: start script

--- start.sh
+++ start.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+if [ "$1" = -h ]; then
+  echo "Usage: $0 [clean]"
+  exit 1
+fi
+
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_NAME="$(basename "${PROJECT_DIR}")"
+
+# stop previous running containers
+docker ps -a | grep "${PROJECT_NAME}" | awk '{print $1}' | xargs docker stop
+
+# remove previous containers
+if [ -n "$(docker ps -a | grep "${PROJECT_NAME}")" ] && [ "$1" == "clean" ]; then
+  docker ps -a | grep "${PROJECT_NAME}" | awk '{print $1}' | xargs docker rm
+fi
 
 set -euo pipefail
 
@@ -11,11 +25,12 @@ else
     dc="docker compose"
 fi
 
-export COMPOSE_FILE="${PROJECT_DIR}/docker-compose.yaml"
-if [ $# -gt 0 ]; then
-    exec $dc run --rm airflow-cli "${@}"
-else
-    exec $dc run --rm airflow-cli
+export AIRFLOW_PROJ_DIR="${PROJECT_DIR}/monorepo/airflow"
+export AIRFLOW_COMPOSE_FILE="${AIRFLOW_PROJ_DIR}/airflow.compose.yml"
+
+if [ ! -f "${AIRFLOW_PROJ_DIR}/.env" ]; then
+  echo -e "AIRFLOW_UID=$(id -u)" > "${AIRFLOW_PROJ_DIR}/.env"
+  echo -e "AIRFLOW_GID=0" >> "${AIRFLOW_PROJ_DIR}/.env"
 fi
 
-# TODO: add compose up with multiple files
+${dc} -p "${PROJECT_NAME}" -f "${AIRFLOW_COMPOSE_FILE}" up -d
