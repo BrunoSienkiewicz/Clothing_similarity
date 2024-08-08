fad9d510f9dc061548109b1d3016571f4dfb9102
Maks <makser.hofman@gmail.com>
2024-08-09 01:28:22 +0200

    UBR-14,20. Lekki update

--- monorepo/airflow/dags/scripts/scrapper_html.py
+++ monorepo/airflow/dags/scripts/scrapper_html.py
@@ -32,6 +32,7 @@ def parse_html(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     feed_items = soup.find_all(class_='feed-item')
     iteration = 0
+    json_array = []
     for feed_item in feed_items:
             saved_links = {}
             iteration += 1
@@ -43,43 +44,45 @@ def parse_html(html_content):
             saved_links['img_link'] = re.sub(r'\?.*$', '', feed_item.find('img', class_='Image-module__crop___nWp1j')['src'])
             saved_links['similar_link'] = GRAILED_BASE_URL + feed_item.find('a', class_='SeeSimilarLink-module__link___tioRk')['href']
             saved_links['time_of_scrapping'] = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
-            save_to_json_file(saved_links, iteration)
+            json_array.append(save_to_json_file(saved_links))
+    return json_array
 
 
-def save_to_json_file(saved_links, iteration):
-    folder_path = 'json_dataset'
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-    filename = f"{saved_links['time_of_scrapping']}_nr{iteration}.json"
-    file_path = os.path.join(folder_path, filename)
-    with open(file_path, 'w') as json_file:
-        json.dump(saved_links, json_file, indent=4)
+def save_to_json_file(saved_links):
+    return json.dump(saved_links, indent=4)
 
-def scrape_html(link, sec_scroll, timeout, categorie):
+def save_to_folder(json_file, filename, folder_name='json_dataset'):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    file_path = os.path.join(folder_name, filename)
+    with open(folder_name, 'W') as file:
+        file.write(json_file)
+    print(f"File saved successfully at {file_path}")
+
+def scrape_html(link=GRAILED_BASE_URL, sec_scroll=5, timeout=0.1, categorie="all"):
     url = link + categorie
     driver = init_driver(url)
     scroll_for_set_amout_of_sec(driver, sec_scroll, timeout)
     parent_element = driver.find_element(By.CLASS_NAME, "feed")
     html_content = parent_element.get_attribute('outerHTML')
-    parse_html(html_content)
+    driver.close()
+    return parse_html(html_content)
 
-def scrape_main_func(link, sec_scroll,timeout, categories):
-    if link is None:
-        link = GRAILED_BASE_URL
-    if sec_scroll is None:
-        sec_scroll = 5
-    if categories is None:
-        categories = ["all"]
-    scrape_html(link, sec_scroll, timeout, categories)
+def main_func_for_users(link, sec_scroll,timeout, categories):
+    json_array = scrape_html(link, sec_scroll, timeout, categories)
+    x = 0
+    for json in json_array:
+        x += 1
+        save_to_folder(json, f"json_file_nr{x}")
 
-def scrape_site():
+def main():
     parser = argparse.ArgumentParser(description="A simple script to demonstrate argparse.")
-    parser.add_argument('-link', '--link', type=str, required=False, help='a link to site defoult https://www.grailed.com/categories ')
-    parser.add_argument('-sec_scroll', '--secScroll', type=int, required=False, help='how long to scroll')
-    parser.add_argument('-timeout', '--timeout', type=int, required=False, help='how long to timeout scroll')
-    parser.add_argument('-categories', '--categories', type=str, required=False, help='pick categories you want to scrape from ')
+    parser.add_argument('-link', '--link', type=str, const=GRAILED_BASE_URL, required=False, help='a link to site defoult https://www.grailed.com/categories ')
+    parser.add_argument('-sec_scroll', '--secScroll', type=int, const=5, required=False, help='how long to scroll')
+    parser.add_argument('-timeout', '--timeout', type=float, const=0.1, required=False, help='how long to timeout scroll')
+    parser.add_argument('-categories', '--categories', type=str, const='all', required=False, help='pick categories you want to scrape from ')
     args = parser.parse_args()
-    scrape_main_func(args.link, args.sec_scroll,args.timeout, args.categories)
+    main_func_for_users(args.link, args.sec_scroll,args.timeout, args.categories)
 
 if __name__ == "__main__":
-    scrape_site()
+    main()
